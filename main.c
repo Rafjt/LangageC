29455a1100e3be749507a6b8a8e4dcd4138f2b9a
Rafael FERNANDO <rafael.fernando@jobteaser.com>
2024-02-07 10:38:23 +0100

    code worktree + card logic

--- main.c
+++ main.c
@@ -1,13 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "card.h"
+#define MAX_CARDS 60
 
-struct carte{
-    char couleur;
-    int nombre;
-    int special;
+void storeCards(struct carte pioche []) {
+    FILE *file = fopen("all_cartes.txt", "r");
+    if (file == NULL) {
+        printf("Could not open file\n");
+        exit(1);
+    }
+
+    int i = 0;
+    while (fscanf(file, " %c %d %d", &pioche [i].couleur, &pioche [i].nombre, &pioche [i].special) == 3) {
+        i++;
+    }
+
+    fclose(file);
 }
 
 int main(){
-      
+    struct carte pioche [MAX_CARDS];
+    storeCards(pioche );
+
+    
+    for (int j = 0; j < MAX_CARDS; j++) {
+        printf("Card %d: %c %d %d\n", j+1, pioche [j].couleur, pioche [j].nombre, pioche [j].special);
+    }
+
+    return 0;
 }
