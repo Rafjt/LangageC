a1a46bfecdad1cfe189b1f1863a74621b2fc2411
Rafael FERNANDO <rafael.fernando@jobteaser.com>
2024-02-18 11:43:16 +0100

    logique joueur OK (reste cartes spéciales)

--- main.c
+++ main.c
@@ -5,7 +5,7 @@
 #include "card.h"
 #include "player.h"
 
-int main(){
+int main() {
     storeCards(pioche);
     int choix;
     int tour = 1;
@@ -17,22 +17,27 @@ int main(){
 
     storeCards(pioche);
     pickSevenCards(pioche, hand, hand2, MAX_CARDS);
+    int cpt_pile = 1;
+    pile[0] = pioche[0];
+    for (int i = 14; i < MAX_CARDS; i++) {
+        pile[i - 13] = pioche[i];
+    }
 
-    int size_hand1 = 7; 
-    int size_hand2 = 7; 
+    int size_hand1 = 7;
+    int size_hand2 = 7;
 
-    while (size_hand1 != 0 && size_hand2 != 0){
+    while (size_hand1 != 0 && size_hand2 != 0) {
         tour = next_tour(tour, direction);
-        printf("la derniere carte jouée est %c %d %d\n", pioche[cpt_pioche].couleur, pioche[cpt_pioche].nombre, pioche[cpt_pioche].special);
+        printf("la derniere carte jouée est %c %d %d\n", pile[cpt_pile - 1].couleur, pile[cpt_pile - 1].nombre, pile[cpt_pile - 1].special);
         player p1 = {1};
         player p2 = {2};
         int player_turn = whichPlayer(tour);
         printf("au tour du joueur n°%d\n", player_turn);
         if (player_turn == 1) {
-            for(int cpt = 0; cpt < size_hand1; cpt++){
-                printf("Hand 1 - Card %d: %c %d %d\n", cpt+1, hand[cpt].couleur, hand[cpt].nombre, hand[cpt].special);
+            for (int cpt = 0; cpt < size_hand1; cpt++) {
+                printf("Hand 1 - Card %d: %c %d %d\n", cpt + 1, hand[cpt].couleur, hand[cpt].nombre, hand[cpt].special);
             }
-            int valid_play = 0; 
+            int valid_play = 0;
             do {
                 printf("Quelle carte voulez-vous jouer ? (Entrez 0 pour piocher une carte)\n");
                 scanf("%d", &choix);
@@ -43,25 +48,31 @@ int main(){
                     break;
                 } else if (choix > size_hand1 || choix <= 0) {
                     printf("Choix invalide\n");
-                } else if (hand[choix-1].nombre == 13 || hand[choix-1].nombre == 14 || hand[choix-1].couleur == pioche[cpt_pioche].couleur || hand[choix-1].nombre == pioche[cpt_pioche].nombre) {
-                    pioche[cpt_pioche] = hand[choix-1]; 
-                    for (int i = choix-1; i < size_hand1; i++){
-                        hand[i] = hand[i+1];
+                } else if (hand[choix - 1].nombre == 13 || hand[choix - 1].nombre == 14 || hand[choix - 1].couleur == pile[cpt_pile - 1].couleur || hand[choix - 1].nombre == pile[cpt_pile - 1].nombre) {
+                    pile[cpt_pile] = hand[choix - 1];
+                    cpt_pile++;
+                    for (int i = choix - 1; i < size_hand1; i++) {
+                        hand[i] = hand[i + 1];
                     }
                     size_hand1--;
-                    printf("la carte jouée est %c %d %d\n", pioche[cpt_pioche].couleur, pioche[cpt_pioche].nombre, pioche[cpt_pioche].special);
+                    printf("la carte jouée est %c %d %d\n", pile[cpt_pile - 1].couleur, pile[cpt_pile - 1].nombre, pile[cpt_pile - 1].special);
                     printf("il vous reste %d cartes\n", size_hand1);
-                    valid_play = 1; 
+                    valid_play = 1;
+                    if (size_hand1 == 1) {
+                        printf("UNO !\n");
+                    } else if (size_hand2 == 0) {
+                        printf("Victoire du Joueur 2 !\n");
+                    }
                 } else {
                     printf("Choix invalide\n");
                 }
-            } while (!valid_play && (choix > size_hand1 || choix <= 0 || !(hand[choix-1].nombre == 13 || hand[choix-1].nombre == 14 || hand[choix-1].couleur == pioche[cpt_pioche].couleur || hand[choix-1].nombre == pioche[cpt_pioche].nombre)));
-        } else if (player_turn == 2) {
+            } while (!valid_play && (choix > size_hand1 || choix <= 0 || !(hand[choix - 1].nombre == 13 || hand[choix - 1].nombre == 14 || hand[choix - 1].couleur == pioche[cpt_pioche].couleur || hand[choix - 1].nombre == pioche[cpt_pioche].nombre)));
+        }
+        else if (player_turn == 2) {
             for (int cpt = 0; cpt < size_hand2; cpt++) {
-                printf("Hand 2 - Card %d: %c %d %d\n", cpt+1, hand2[cpt].couleur, hand2[cpt].nombre, hand2[cpt].special);
+                printf("Hand 2 - Card %d: %c %d %d\n", cpt + 1, hand2[cpt].couleur, hand2[cpt].nombre, hand2[cpt].special);
             }
-
-            int valid_play = 0; 
+            int valid_play = 0;
             do {
                 printf("Quelle carte voulez-vous jouer ? (Entrez 0 pour piocher une carte)\n");
                 scanf("%d", &choix);
@@ -72,22 +83,25 @@ int main(){
                     break;
                 } else if (choix > size_hand2 || choix <= 0) {
                     printf("Choix invalide\n");
-                } else if (hand2[choix-1].nombre == 13 || hand2[choix-1].nombre == 14 || hand2[choix-1].couleur == pioche[cpt_pioche].couleur || hand2[choix-1].nombre == pioche[cpt_pioche].nombre) {
-                    pioche[cpt_pioche] = hand2[choix-1]; 
-
-                    for (int i = choix-1; i < size_hand2; i++) {
-                        hand2[i] = hand2[i+1];
+                } else if (hand2[choix - 1].nombre == 13 || hand2[choix - 1].nombre == 14 || hand2[choix - 1].couleur == pile[cpt_pile - 1].couleur || hand2[choix - 1].nombre == pile[cpt_pile - 1].nombre) {
+                    pile[cpt_pile] = hand2[choix - 1];
+                    cpt_pile++;
+                    for (int i = choix - 1; i < size_hand2; i++) {
+                        hand2[i] = hand2[i + 1];
                     }
-
                     size_hand2--;
-
-                    printf("La carte jouée est %c %d %d\n", pioche[cpt_pioche].couleur, pioche[cpt_pioche].nombre, pioche[cpt_pioche].special);
-                    printf("Il vous reste %d cartes\n", size_hand2);
-                    valid_play = 1; 
+                    printf("la carte jouée est %c %d %d\n", pile[cpt_pile - 1].couleur, pile[cpt_pile - 1].nombre, pile[cpt_pile - 1].special);
+                    printf("il vous reste %d cartes\n", size_hand2);
+                    valid_play = 1;
+                    if (size_hand2 == 1) {
+                        printf("UNO !\n");
+                    } else if (size_hand2 == 0) {
+                        printf("Victoire du Joueur 2 !\n");
+                    }
                 } else {
                     printf("Choix invalide\n");
                 }
-            } while (!valid_play && (choix > size_hand2 || choix <= 0 || !(hand2[choix-1].nombre == 13 || hand2[choix-1].nombre == 14 || hand2[choix-1].couleur == pioche[cpt_pioche].couleur || hand2[choix-1].nombre == pioche[cpt_pioche].nombre)));
+            } while (!valid_play && (choix > size_hand2 || choix <= 0 || !(hand2[choix - 1].nombre == 13 || hand2[choix - 1].nombre == 14 || hand2[choix - 1].couleur == pioche[cpt_pioche].couleur || hand2[choix - 1].nombre == pioche[cpt_pioche].nombre)));
+        }
         }
     }
-}
