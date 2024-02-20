de2dfbfe51186776fc80c8ec3f8beb6bc61179bc
Rafael FERNANDO <rafael.fernando@jobteaser.com>
2024-02-20 22:46:42 +0100

    basic game OK (peaufiner l'UX et rajouter net)

--- gui.h
+++ gui.h
@@ -1,7 +1,14 @@
 #ifndef GUI_H
 #define GUI_H
+#include <SDL2/SDL.h>
+#include "/usr/local/include/SDL2/SDL_image.h"
+#include <SDL2/SDL_ttf.h>
+#include "card.h"
 
 void displayGUI();
-void displayCards();
+char* getHandImageFilename(carte hand);
+SDL_Texture* loadHandImage(SDL_Renderer* renderer, carte hand);
+void drawHand(SDL_Renderer* renderer, carte hand, int x, int y);
+
 
 #endif
--- main.c
+++ main.c
@@ -5,17 +5,41 @@
 #include "card.h"
 #include "player.h"
 #include "gui.h"
+#define HAND_WIDTH 50
+#define HAND_HEIGHT 75
 
 int main() {
+    SDL_Window* window = SDL_CreateWindow("Game", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 800, 600, SDL_WINDOW_SHOWN);
+    if (window == NULL) {
+        printf("Could not create window: %s\n", SDL_GetError());
+        SDL_Quit();
+        return 1;
+    }
+
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
+    if (renderer == NULL) {
+        printf("Could not create renderer: %s\n", SDL_GetError());
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
+
+    // Set the color for drawing operations (RGBA: 53, 101, 77, 255)
+    SDL_SetRenderDrawColor(renderer, 53, 101, 77, 255);
+
+    // Clear the current rendering target with the drawing color
+    SDL_RenderClear(renderer);
+
+    // Update the screen with any rendering performed since the previous call
+    SDL_RenderPresent(renderer);
+
     storeCards(pioche);
     int choix;
     int cpt_pioche = 17;
     TourDirection td = {1, 0};  
+    int valid_play = 0;
 
     displayGUI();
-
-    // pour apppeler pickCard pour le joueur 1 = pickCard(pioche, &cpt_pioche, hand, &size_hand1);
-    // pour apppeler pickCard pour le joueur 2 = pickCard(pioche, &cpt_pioche, hand2, &size_hand2);
 
     storeCards(pioche);
     pickSevenCards(pioche, hand, hand2, MAX_CARDS);
@@ -25,18 +49,19 @@ int main() {
     int size_hand1 = 7;
     int size_hand2 = 7;
 
-    while (size_hand1 != 0 && size_hand2 != 0) {
-        td = next_tour(td.tour, td.direction);
+     while (size_hand1 != 0 && size_hand2 != 0) {
+        SDL_RenderClear(renderer);
         printf("la derniere carte jouée est %c %d %d\n", pile[cpt_pile - 1].couleur, pile[cpt_pile - 1].nombre, pile[cpt_pile - 1].special);
         player p1 = {1};
         player p2 = {2};
         int player_turn = whichPlayer(td);
         printf("au tour du joueur n°%d\n", player_turn);
         if (player_turn == 1) {
-            for (int cpt = 0; cpt < size_hand1; cpt++) {
-                printf("Hand 1 - Card %d: %c %d %d\n", cpt + 1, hand[cpt].couleur, hand[cpt].nombre, hand[cpt].special);
-            }
             int valid_play = 0;
+            for (int cpt = 0; cpt < size_hand1; cpt++) {
+                drawHand(renderer, hand[cpt], cpt * HAND_WIDTH, 0);
+            }
+            SDL_RenderPresent(renderer);
             do {
                 printf("Quelle carte voulez-vous jouer ? (Entrez 0 pour piocher une carte)\n");
                 scanf("%d", &choix);
@@ -51,10 +76,12 @@ int main() {
                     pile[cpt_pile] = hand[choix - 1];
                     cpt_pile++;
                     for (int i = choix - 1; i < size_hand1; i++) {
+                        printf("hand[%d] = %c %d %d\n", i+1, hand[i].couleur, hand[i].nombre, hand[i].special);
                         hand[i] = hand[i + 1];
                     }
                     size_hand1--;
                     printf("la carte jouée est %c %d %d\n", pile[cpt_pile - 1].couleur, pile[cpt_pile - 1].nombre, pile[cpt_pile - 1].special);
+                    printf("\ntour avant = %d\n", td.tour);
                     // After a card has been played:
                     if (pile[cpt_pile - 1].special == 1) {
                         switch (pile[cpt_pile - 1].nombre) {
@@ -62,10 +89,10 @@ int main() {
                                 pickTwoCards(pioche, &cpt_pioche, hand2, &size_hand2);
                                 break;
                             case 11:
-                                td = reverse_tour(td.tour, td.direction);
+                                td = reverse_tour(td.tour,td.direction);
                                 break;
                             case 12:
-                                td = skip_tour(td.tour, td.direction);
+                                td = skip_tour(td.tour,td.direction);
                                 break;
                             case 13:
                                 changeColor(pile, &cpt_pile);
@@ -74,6 +101,7 @@ int main() {
                                 pickFourCards(pioche, &cpt_pioche, hand2, &size_hand2);
                                 break;
                         }
+                        printf("\ntour après = %d\n", td.tour);
                     }
                     printf("il vous reste %d cartes\n", size_hand1);
                     valid_play = 1;
@@ -86,12 +114,13 @@ int main() {
                     printf("Choix invalide\n");
                 }
             } while (!valid_play && (choix > size_hand1 || choix <= 0 || !(hand[choix - 1].nombre == 13 || hand[choix - 1].nombre == 14 || hand[choix - 1].couleur == pioche[cpt_pioche].couleur || hand[choix - 1].nombre == pioche[cpt_pioche].nombre)));
-        }
-        else if (player_turn == 2) {
-            for (int cpt = 0; cpt < size_hand2; cpt++) {
-                printf("Hand 2 - Card %d: %c %d %d\n", cpt + 1, hand2[cpt].couleur, hand2[cpt].nombre, hand2[cpt].special);
-            }
+            int player_turn = whichPlayer(td);
+        }else if (player_turn == 2) {
             int valid_play = 0;
+            for (int cpt = 0; cpt < size_hand2; cpt++) {
+                drawHand(renderer, hand2[cpt], cpt * HAND_WIDTH, 0);
+            }
+            SDL_RenderPresent(renderer);
             do {
                 printf("Quelle carte voulez-vous jouer ? (Entrez 0 pour piocher une carte)\n");
                 scanf("%d", &choix);
@@ -107,13 +136,15 @@ int main() {
                     cpt_pile++;
                     for (int i = choix - 1; i < size_hand2; i++) {
                         hand2[i] = hand2[i + 1];
+                        printf("hand2[%d] = %c %d %d\n", i+1, hand2[i].couleur, hand2[i].nombre, hand2[i].special);
                     }
                     size_hand2--;
                     printf("la carte jouée est %c %d %d\n", pile[cpt_pile - 1].couleur, pile[cpt_pile - 1].nombre, pile[cpt_pile - 1].special);
+                    printf("\ntour avant = %d\n", td.tour);
                     if (pile[cpt_pile - 1].special == 1) {
                         switch (pile[cpt_pile - 1].nombre) {
                             case 10:
-                                pickTwoCards(pioche, &cpt_pioche, hand2, &size_hand2);
+                                pickTwoCards(pioche, &cpt_pioche, hand, &size_hand1);
                                 break;
                             case 11:
                                 td = reverse_tour(td.tour, td.direction);
@@ -125,9 +156,10 @@ int main() {
                                 changeColor(pile, &cpt_pile);
                                 break;
                             case 14:
-                                pickFourCards(pioche, &cpt_pioche, hand2, &size_hand2);
+                                pickFourCards(pioche, &cpt_pioche, hand, &size_hand1);
                                 break;
                         }
+                        printf("\ntour après = %d\n", td.tour);
                     }
                     printf("il vous reste %d cartes\n", size_hand2);
                     valid_play = 1;
@@ -141,5 +173,12 @@ int main() {
                 }
             } while (!valid_play && (choix > size_hand2 || choix <= 0 || !(hand2[choix - 1].nombre == 13 || hand2[choix - 1].nombre == 14 || hand2[choix - 1].couleur == pioche[cpt_pioche].couleur || hand2[choix - 1].nombre == pioche[cpt_pioche].nombre)));
         }
-        }
-    }
+        td = next_tour(td.tour, td.direction);
+     }
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return 0;
+}
