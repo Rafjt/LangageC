99bee0eba1ac84ffcc6a2aab0e7b246965a1f7c8
Rafael FERNANDO <rafael.fernando@jobteaser.com>
2024-02-20 22:46:42 +0100

    basic game OK (peaufiner l'UX et rajouter net)

--- game.c
+++ game.c
@@ -8,14 +8,14 @@ TourDirection next_tour(int tour, int direction) {
     TourDirection td;
     td.direction = direction;  // direction doesn't change in next_tour
     if (direction == 1) {
-        td.tour = tour - 1;
+        td.tour = (tour - 1 + 2) % 2;
     } else {
-        td.tour = tour + 1;
+        td.tour = (tour + 1) % 2;
     }
     return td;
 }
 
-TourDirection skip_tour(int tour, int direction) {
+TourDirection skip_tour( int tour, int direction) {
     TourDirection td;
     td.direction = direction;  // direction doesn't change in skip_tour
     if (direction == 1) {
@@ -30,19 +30,17 @@ TourDirection reverse_tour(int tour, int direction) {
     TourDirection td;
     if (direction == 0) {
         td.direction = 1;
-        td.tour = tour - 1;
     } else {
         td.direction = 0;
-        td.tour = tour + 1;
     }
     return td;
 }
 
 int whichPlayer(TourDirection td) {
     if (td.tour % 2 == 0) {
-        return 1; 
-    } else {
         return 2; 
+    } else {
+        return 1; 
     }
 }
 
--- gui.c
+++ gui.c
@@ -1,20 +1,24 @@
 #include "gui.h"
 #include <SDL2/SDL.h>
 #include "/usr/local/include/SDL2/SDL_image.h"
+#include <SDL2/SDL_ttf.h>
+#include "card.h"
+#define HAND_WIDTH 50
+#define HAND_HEIGHT 75
 
 void displayGUI() {
-    SDL_Window *window;                    
+    SDL_Window *window;
     SDL_Renderer* renderer;
 
-    SDL_Init(SDL_INIT_VIDEO);              
+    SDL_Init(SDL_INIT_VIDEO);
 
     window = SDL_CreateWindow(
-        "An SDL2 window",                  
-        SDL_WINDOWPOS_UNDEFINED,           
-        SDL_WINDOWPOS_UNDEFINED,           
-        640,                               
-        480,                               
-        SDL_WINDOW_OPENGL                  
+        "An SDL2 window",
+        SDL_WINDOWPOS_UNDEFINED,
+        SDL_WINDOWPOS_UNDEFINED,
+        640,
+        480,
+        SDL_WINDOW_OPENGL
     );
 
     if (window == NULL) {
@@ -25,49 +29,71 @@ void displayGUI() {
     renderer = SDL_CreateRenderer(window, -1, 0);
     SDL_SetRenderDrawColor(renderer, 53, 101, 77, 255); // Set renderer color to poker green
 
-    SDL_Surface* cardImage = IMG_Load("1.png");
-    if (!cardImage) {
-        printf("IMG_Load: %s\n", IMG_GetError());
-        // handle error
-    }
-
-    // Create a texture from the image
-    SDL_Texture* cardTexture = SDL_CreateTextureFromSurface(renderer, cardImage);
-    if (!cardTexture) {
-        printf("SDL_CreateTextureFromSurface: %s\n", SDL_GetError());
-        // handle error
-    }
-
-    // Free the loaded image. We don't need it anymore since we now have a texture
-    SDL_FreeSurface(cardImage);
-
-    // Define where we want to draw the card. For example, at position (50, 50) and with size (100, 150)
-    SDL_Rect cardRect;
-    cardRect.x = 50;
-    cardRect.y = 50;
-    cardRect.w = 100;
-    cardRect.h = 150;
-
     // Clear the renderer with the draw color
     SDL_RenderClear(renderer);
 
-    // Draw the card
-    SDL_RenderCopy(renderer, cardTexture, NULL, &cardRect);
-
-    // Update the screen with our rendered image
+    // Update the screen with our rendered background
     SDL_RenderPresent(renderer);
 
     SDL_Event event;
     int running = 1;
     while (running) {
         while (SDL_PollEvent(&event)) {
-            if (event.type == SDL_QUIT) {
-                running = 0;
+            switch (event.type) {
+                case SDL_QUIT:
+                    running = 0;
+                    break;
+                case SDL_KEYDOWN:
+                    if (event.key.keysym.sym == SDLK_RETURN || event.key.keysym.sym == SDLK_RETURN2) {
+                        running = 0;
+                    }
+                    break;
             }
         }
     }
-    SDL_DestroyTexture(cardTexture);
+
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
+}
+
+
+
+
+char* getHandImageFilename(carte hand) {
+    char* filename = malloc(20 * sizeof(char));
+    sprintf(filename, "card_img/%c_%d_%d.png", hand.couleur, hand.nombre, hand.special);
+    return filename;
+}
+
+SDL_Texture* loadHandImage(SDL_Renderer* renderer, carte hand) {
+    char* filename = getHandImageFilename(hand);
+    SDL_Surface* surface = IMG_Load(filename);
+    SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
+    SDL_FreeSurface(surface);
+    free(filename);
+    return texture;
+}
+
+void drawHand(SDL_Renderer* renderer, carte hand, int x, int y) {
+    // Check if renderer is NULL
+    if (renderer == NULL) {
+        printf("Renderer is NULL\n");
+        return;
+    }
+
+    // Create a texture for the hand
+    SDL_Texture* texture = loadHandImage(renderer, hand);
+    if (texture == NULL) {
+        printf("Could not create texture: %s\n", SDL_GetError());
+        return;
+    }
+
+    SDL_Rect rect;
+    rect.x = x;
+    rect.y = y;
+    rect.w = HAND_WIDTH;  
+    rect.h = HAND_HEIGHT;
+    SDL_RenderCopy(renderer, texture, NULL, &rect);
+    SDL_DestroyTexture(texture);
 }
